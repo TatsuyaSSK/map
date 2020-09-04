aa34db6ad577bb78d0c1b788c6e3406ef02dbe9e
Tatsuya Sasaki <ssktty514@gmail.com>
2020-09-04 14:05:36 +0900

    map機能を修正

--- mapapp/views.py
+++ mapapp/views.py
@@ -2,28 +2,30 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 import json
+import geocoder
 
 # Create your views here.
 
 def mainfunc(request): 
     if request.method == "POST":
-        #駅、チェックボックスのあるなしで、どのjsonファイルを開くか限定する
-        station = request.POST["station"]
+        print(request.POST)
+        #住所を座標に変換
+        address = request.POST["address"]
+        location_list = geocoder.osm(address, timeout=5.0).latlng
+        location = (location_list[0],location_list[1])
+        #stationには座標が格納されている
         lessons = request.POST.getlist("lessons")
         json_files = []
+        #swimming.jsonの中から、距離が20分以内のものだけ表示、みたいなことをする
         for lesson in lessons:
-            json_open = open("static/{station}/{lesson}.json".format(station = station,lesson = lesson),"r")
+            json_open = open("static/{lesson}.json".format(lesson = lesson),"r")
             json_load = json.load(json_open)
-            json_dumps = json.dumps(json_load[0:30], ensure_ascii=False)
-            #試しに10個渡す
+            #json形式に変換
+            json_dumps = json.dumps(json_load,ensure_ascii=False)
             json_files.append(json_dumps)
-
+            
         #辞書型配列を作成
-        dic = dict(zip(lessons,json_files))
-        dic["station"] = request.POST["station"]
-        dic["walktime"] = request.POST["walktime"]
-        dic["lessons"] = request.POST.getlist("lessons")
-
+        dic = dict(jsons=json_files,lessons = request.POST.getlist("lessons"),location = location,address = address)
         #map.htmlに渡す
         return render(request,"map.html",dic)
         
@@ -32,4 +34,4 @@ def mainfunc(request):
 def mapfunc(request):
     if request.method == "POST":
         return redirect("main")
-    return render(request,"map.html",{"station":station})
+    return render(request,"map.html")
